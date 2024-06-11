dbb4440be07adb62b46a96fe7abe6fd51edc8f55
Kaden Zheng <KadenZheng@mail.fresnostate.edu>
2024-06-11 14:31:00 +0200

    update

--- src/background.js
+++ src/background.js
@@ -1,70 +1,72 @@
+/* global chrome */
+
 let accumulatedData = [];
 
 chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message.data) {
-    let filteredSignalData = JSON.parse(message.data);
-    let diiSignalData = filteredSignalData["DII"];
-    console.log("Received data:", diiSignalData);
+    if (message.data) {
+        let filteredSignalData = JSON.parse(message.data);
+        let diiSignalData = filteredSignalData["DII"];
+        // console.log("Received data:", diiSignalData);
 
-    if (diiSignalData) {
-      accumulatedData = accumulatedData.slice(-2500); // Remove old points
-      accumulatedData.push(...diiSignalData);
-      console.log("accumulated data:", accumulatedData.length);
+        if (diiSignalData) {
+            accumulatedData = accumulatedData.slice(-2500); // Remove old points
+            accumulatedData.push(...diiSignalData);
+            // console.log("accumulated data:", accumulatedData.length);
 
-      if (accumulatedData.length >= 2500) {
-        console.log("enough data");
-        console.log(accumulatedData);
-        // Send the DII signal data to the Python server
-        fetch("http://localhost:5000/calculate_heart_rate", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            dii_signal: accumulatedData.slice(-2500), // Changed this line
-            fs: 1000,
-          }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.error) {
-              console.error("Error:", data.error);
-              sendResponse({ error: data.error });
+            if (accumulatedData.length >= 2500) {
+                console.log("enough data");
+                // console.log(accumulatedData);
+                // Send the DII signal data to the Python server
+                fetch("http://localhost:5000/calculate_heart_rate", {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        dii_signal: accumulatedData.slice(-2500), // Changed this line
+                        fs: 1000,
+                    }),
+                })
+                    .then((response) => response.json())
+                    .then((data) => {
+                        if (data.error) {
+                            console.error("Error:", data.error);
+                            sendResponse({ error: data.error });
+                        } else {
+                            console.log(data.heart_rate.toFixed(2));
+                            chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+                                if (tabs && tabs.length > 0) {
+                                    chrome.tabs.sendMessage(tabs[0].id, { heart_rate: data.heart_rate.toFixed(2) });
+                                } else {
+                                    console.error("No active tab found in the current window.");
+                                }
+                            });
+                            sendResponse({ heart_rate: data.heart_rate.toFixed(2), r_peaks: data.r_peaks });
+                        }
+                    })
+                    .catch((error) => {
+                        console.error("Error:", error);
+                        sendResponse({ error: error.message });
+                    })
+                    .finally(() => {
+                        // Ensure the message channel is closed
+                        return true;
+                    });
+
+                // Remove the sent data from the accumulated data
+                accumulatedData = accumulatedData.slice(2500);
+                // console.log("Data after slicing:", accumulatedData.length); // Add this line
             } else {
-              console.log(data.heart_rate.toFixed(2));
-              chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                if (tabs && tabs.length > 0) {
-                  chrome.tabs.sendMessage(tabs[0].id, { heart_rate: data.heart_rate.toFixed(2) });
-                } else {
-                  console.error("No active tab found in the current window.");
-                }
-              });
-              sendResponse({ heart_rate: data.heart_rate.toFixed(2), r_peaks: data.r_peaks });
+                sendResponse({ message: "Not enough data accumulated yet" });
             }
-          })
-          .catch((error) => {
-            console.error("Error:", error);
-            sendResponse({ error: error.message });
-          })
-          .finally(() => {
-            // Ensure the message channel is closed
-            return true;
-          });
-
-        // Remove the sent data from the accumulated data
-        accumulatedData = accumulatedData.slice(2500);
-        console.log("Data after slicing:", accumulatedData.length); // Add this line
-      } else {
-        sendResponse({ message: "Not enough data accumulated yet" });
-      }
+        } else {
+            console.log("DII signal not found in the data");
+            sendResponse({ error: "DII signal not found in the data" });
+        }
     } else {
-      console.log("DII signal not found in the data");
-      sendResponse({ error: "DII signal not found in the data" });
+        console.log("EXTENSION: Could not receive filteredData");
+        sendResponse({ error: "Could not receive filteredData" });
     }
-  } else {
-    console.log("EXTENSION: Could not receive filteredData");
-    sendResponse({ error: "Could not receive filteredData" });
-  }
 
-  return true; // Keep the messaging channel open for async response
+    return true; // Keep the messaging channel open for async response
 });
--- src/content.js
+++ src/content.js
@@ -1,30 +1,32 @@
+/* global chrome */
+
 // Add an event listener to handle incoming messages
 window.addEventListener("message", (event) => {
-  // Ensure the message is from the same window
-  if (event.source !== window) return;
-  switch (event.data.type) {
-    case "DATA_RESPONSE":
-      console.log("EXTENSION: DATA_RESPONSE received:", event.data.payload);
-      break;
-    case "FILTERED_DATA":
-      if (!window.heartRateInterval) {
-        console.log("setting interval");
-        window.heartRateInterval = setInterval(() => {
-          const filteredSignalData = localStorage.getItem("Filtered Signal Data");
-          console.log("Retrieved from localStorage:", filteredSignalData);
-          if (filteredSignalData) {
-            console.log("Sending data:", filteredSignalData);
-            chrome.runtime.sendMessage({ data: filteredSignalData });
-          }
-        }, 25);
-      }
-      break;
-    case "CONFIG_DATA":
-      console.log("EXTENSION: CONFIG_DATA received:", event.data.payload);
-      break;
-    default:
-      console.log("EXTENSION: Unknown message type:", event.data.type);
-  }
+    // Ensure the message is from the same window
+    if (event.source !== window) return;
+    switch (event.data.type) {
+        case "DATA_RESPONSE":
+            console.log("EXTENSION: DATA_RESPONSE received:", event.data.payload);
+            break;
+        case "FILTERED_DATA":
+            if (!window.heartRateInterval) {
+                console.log("setting interval");
+                window.heartRateInterval = setInterval(() => {
+                    const filteredSignalData = localStorage.getItem("Filtered Signal Data");
+                    // console.log("Retrieved from localStorage:", filteredSignalData);
+                    if (filteredSignalData) {
+                        // console.log("Sending data:", filteredSignalData);
+                        chrome.runtime.sendMessage({ data: filteredSignalData });
+                    }
+                }, 25);
+            }
+            break;
+        case "CONFIG_DATA":
+            // console.log("EXTENSION: CONFIG_DATA received:", event.data.payload);
+            break;
+        default:
+        // console.log("EXTENSION: Unknown message type:", event.data.type);
+    }
 });
 
 // Request the Config upon loading of the Chrome extension
@@ -34,19 +36,20 @@ window.addEventListener("message", (event) => {
 // });
 
 chrome.runtime.onMessage.addListener((message) => {
-  if (message.heart_rate) {
-    let heartRateElement = document.getElementById("heart-rate");
-    if (!heartRateElement) {
-      heartRateElement = document.createElement("div");
-      heartRateElement.id = "heart-rate";
-      heartRateElement.style.position = "fixed";
-      heartRateElement.style.top = "10px";
-      heartRateElement.style.right = "10px";
-      heartRateElement.style.backgroundColor = "white";
-      heartRateElement.style.padding = "10px";
-      heartRateElement.style.border = "1px solid black";
-      document.body.appendChild(heartRateElement);
+    if (message.heart_rate) {
+        console.log(message.heart_rate);
+        let heartRateElement = document.getElementById("heart-rate");
+        if (!heartRateElement) {
+            heartRateElement = document.createElement("div");
+            heartRateElement.id = "heart-rate";
+            heartRateElement.style.position = "fixed";
+            heartRateElement.style.top = "10px";
+            heartRateElement.style.right = "10px";
+            heartRateElement.style.backgroundColor = "white";
+            heartRateElement.style.padding = "10px";
+            heartRateElement.style.border = "1px solid black";
+            document.body.appendChild(heartRateElement);
+        }
+        heartRateElement.textContent = `Heart Rate: ${message.heart_rate} BPM`;
     }
-    heartRateElement.textContent = `Heart Rate: ${message.heart_rate} BPM`;
-  }
 });
